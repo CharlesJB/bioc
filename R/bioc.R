10fcee9117e0964052f6ddda7005fb14ba151945
Charles Joly Beauparlant <Charles.Joly-Beauparlant@crchul.ulaval.ca>
2015-10-21 10:43:30 -0400

    Changed the interface of the bioc function.
    Replaced the ... by a `pkg` argument. This remove the possibility of
    using extra arguments for biocLite, but it should now work to install a
    package with a simple bioc("name_of_the_package").
    
    Signed-off-by: Charles Joly Beauparlant <Charles.Joly-Beauparlant@crchul.ulaval.ca>

--- R/bioc.R
+++ R/bioc.R
@@ -5,18 +5,19 @@
 #'
 #' biocLite script is sourced from http://www.bioconductor.org/biocLite.R
 #'
+#' @param pkg The name of the package to install. If \code{NULL}, the function
+#'            will run \code{biocLite()} which will update all packages.
 #' @param force_source \code{source} the biocLite.R script even if a biocLite
 #'                     function is already available. Default: \code{FALSE}.
-#' @param ... Argument to pass to the biocLite function.
 #'
 #' @examples
 #' bioc() # To update all packages
 #' bioc("metagene") # To install a Bioconductor's package
 #'
 #' @export
-bioc <- function(force_source = FALSE, ...) {
+bioc <- function(pkg = NULL, force_source = FALSE) {
     if (force_source == TRUE | ! exists("biocLite")) {
         source("http://www.bioconductor.org/biocLite.R")
     }
-    biocLite(...)
+    biocLite(pkg)
 }
